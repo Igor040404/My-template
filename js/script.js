06864fcbc0c86f34567416f5351e816c4a3048c8
TikTak04 <orishchak.igor@meta.ua>
2019-09-13 12:35:15 +0300

    first draft

---
+++ js/script.js
@@ -0,0 +1,10 @@
+(function ($) {
+	$(function (){
+		$('.icon').on('click', function (){
+			$(this).closest('.menu-burger').toggleClass('menu-open');
+			// body...
+		});
+		// body...
+	});
+	// body...
+})(jQuery);
